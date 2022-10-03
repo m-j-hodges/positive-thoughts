cf21ca55d2eeeedfa24798efaae5cd411f38bdcf
Sean Conrad <sean.w.conrad@gmail.com>
2022-10-03 06:51:01 -0400

    added additional folders for base structure of the project

--- server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const db = require('./config/connection');
-
-c
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+code taken from lesson 14 in week 21 01/activities 
+type Profile {
+    _id: ID
+    name: String
+    skills: [String]!
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+  }
+
+  type Mutation {
+    addProfile(name: String!): Profile
+    addSkill(profileId: ID!, skill: String!): Profile
+    removeProfile(profileId: ID!): Profile
+    removeSkill(profileId: ID!, skill: String!): Profile
+  }
+
+
+`;
