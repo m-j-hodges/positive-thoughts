b2a5bc21d1b9ae25fa6bcc875b74098618029abc
Jason South <jason.south@me.com>
2022-10-10 18:58:50 -0500

    few changes trying to pull

--- client/src/components/thoughtsFeed.js
+++ client/src/components/thoughtsFeed.js
@@ -1,27 +1,66 @@
 import React from 'react'
-import axios from 'axios'
+import Axios from 'axios'
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react'
+import "./ThoughtsCss.css"
 
-async function Thoughts({loggedIn}) {
+
+
+function Thoughts() {
   //  if(loggedIn) {
-    
-    const fetch_url = 'https://zenquotes.io/api/quotes'
-    const response = await axios.get(fetch_url)
-    const data = await response.json()
+const [newFact, setFact] = useState('')
+// const [author, setAuthor] = useState('')
+let cardString = []
+const fetchData = () => {
 
-  console.log(data)
+const cachesQuotes = localStorage.getItem('quotes')
+let divArray
+let saveQuotes
+if(!cachesQuotes) {
+  
+const fetchResult = Axios.get('https://type.fit/api/quotes')
+  .then((res)=> {
+    console.log(res)
+    console.log(res.data)
+    divArray = res.data
+    divArray.length = 20
+    saveQuotes =localStorage.setItem('quotes', JSON.stringify(divArray))
+  })}
 
-    const relevantData = data.map((item) => {
-    return `<div class="card">
-    <div class="card-body">
-      "${item.q}"
-      --${item.a}
-    </div>
-  </div>`
+  divArray = JSON.parse(cachesQuotes)
+    const newArray = divArray.forEach((item) => {
+    const writeQuotes = document.getElementById('quotesDiv')
+    writeQuotes.innerHTML += `<div class="card my-4">
+      <div class="card-header">
+        Quote
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p>${item.text}</p>
+          <footer class="blockquote-footer">${item.author} <cite title="Source Title"></cite></footer>
+        </blockquote>
+      </div>
+    </div>`
+    return cardString
     })
+    console.log(newArray)
+  
+  
+}
+  //   const relevantData = data.map((item) => {
+  //   return `<div class="card">
+  //   <div class="card-body">
+  //     "${item.q}"
+  //     --${item.a}
+  //   </div>
+  // </div>`
+  //   })
   // }
+ 
     return (
       <div>
-      {relevantData}
+      <div id="quotesDiv"></div>
+      <button onClick={fetchData}>Click Me!</button> 
       </div>
     )
 
