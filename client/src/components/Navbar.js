c4c4aed94daf4e09360c054ff70b71468d55e22b
Jason South <jason.south@me.com>
2022-10-09 22:28:36 -0500

    signUp page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,19 @@
-import React from 'react'
-
+import React from 'react';
 
 function Navbar() {
   return (
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Positive Thoughts</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">Positive Thoughts</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/Home">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="/profile">Profile</a>
-            <a class="nav-item nav-link" href="/signup">Sign Up</a>
-            <a class="nav-item nav-link" href="/signup">Login</a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="/Home">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-item nav-link" href="/profile">Profile</a>
+            <a className="nav-item nav-link" href="/signup">Sign Up</a>
+            <a className="nav-item nav-link" href="/signup">Login</a>
             </div>
         </div>
     </nav>
