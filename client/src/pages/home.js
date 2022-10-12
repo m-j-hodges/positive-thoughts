b5009be54c5c773ac266d9398eaa826741571c58
Sean Conrad <sean.w.conrad@gmail.com>
2022-10-11 20:19:32 -0400

    added background image to home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Thoughts from '../components/thoughtsFeed'
-//import backgroundImage from '../../src/img/stacked_rocks.png';
+import Thoughts from '../components/thoughtsFeed';
+import  '../styles/app.css';
+
 
 const Home = () => {
 
   return (
-    <div>
+    <div className="photo">
 <Navbar />
 <Thoughts />
 </div>
