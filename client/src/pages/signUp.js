56ea076d0d757d8ef7e06af9617febceabfd23c4
Jason South <jason.south@me.com>
2022-10-15 20:31:48 -0500

    changed format & added logo

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/signUp.css';
-
+import image from '../images/shutterstock_299372690.jpg'
 import {
   MDBBtn,
   MDBContainer,
   MDBCard,
   MDBCardBody,
+  MDBCardImage,
   MDBInput,
-  MDBIcon,
+  // MDBIcon,
   MDBRow,
   MDBCol
 }
@@ -16,64 +17,60 @@ from 'mdb-react-ui-kit';
 function signUp() {
   return (
     <MDBContainer fluid className='my-5'>
-
       <MDBRow className='g-0 align-items-center'>
         <MDBCol size='lg' col='6'>
-
           <MDBCard className='my-5' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
             <MDBCardBody className='p-5 shadow-5 text-center'>
-
-              <h2 className="fw-bold mb-2">Sign up</h2>
-              <h5 className='mb-5'>to receive inspiring quotes</h5>
-
-              <MDBRow>
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+              <MDBRow className='g-0 align-items-center'>
+                {/* <MDBCol size='lg' md='5' col='6'> */}
+                {/* logo image */}
+                <MDBCol className='mr-5 mb-3' md='5' col='4'>
+                  <MDBCardImage src={image}  className='w-0' fluid />
                 </MDBCol>
-
-                <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                <MDBCol>    
+                  <h2 className="fw-bold mb-2">Sign up</h2>
+                  <h5 className='mb-5'>to receive inspiring quotes</h5>
+                  <MDBRow>
+                    <MDBCol col='4'>
+                      <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text'/>
+                    </MDBCol>
+                  
+                    <MDBCol col='4'>
+                      <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text'/>
+                    </MDBCol>
+                  </MDBRow>
+                    <MDBInput wrapperClass='mb-4' label='email' id='typeEmail' type='email'/>
+                    <MDBInput wrapperClass='mb-4' label='Password' id='typePassword' type='password'/>
+                    <MDBBtn className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
                 </MDBCol>
               </MDBRow>
-              
-              <MDBInput wrapperClass='mb-4' label='email' id='typeEmail' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='Password' id='typePassword' type='password'/>
 
-              <MDBBtn className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
+              {/* future development for SSO */}
+              {/* <div className="text-center">
 
-              <div className="text-center">
+              <p>or sign up with:</p>
 
-                <p>or sign up with:</p>
+              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+              <MDBIcon fab icon='facebook-f' size="sm"/>
+              </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='facebook-f' size="sm"/>
-                </MDBBtn>
+              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+              <MDBIcon fab icon='twitter' size="sm"/>
+              </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='twitter' size="sm"/>
-                </MDBBtn>
+              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+              <MDBIcon fab icon='google' size="sm"/>
+              </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='google' size="sm"/>
-                </MDBBtn>
-
-                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
-                  <MDBIcon fab icon='github' size="sm"/>
-                </MDBBtn>
-              </div>
+              <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+              <MDBIcon fab icon='github' size="sm"/>
+              </MDBBtn>
+              </div> */}
 
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-
-        {/* image with logo above....still working on logo */}
-        <MDBCol size='lg' col='6'>
-          <p className='text-center'>Positive Thoughts</p>
-        <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid w-100 rounded shadow-4' alt='' fluid/>
-        </MDBCol>
-
-      </MDBRow>
-
+      </MDBRow> 
     </MDBContainer>
   );
 }
