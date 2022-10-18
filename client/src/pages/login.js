9234421432fe58edd3976611904f8cc3393bad17
Lucia Gil <luciagil5952@gmail.com>
2022-10-17 23:27:03 -0400

    added to login js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import image from '../images/PositiveThoughts.bmp'
 import {
   MDBBtn,
@@ -12,64 +12,56 @@ import {
   MDBInput,
   MDBCheckbox,
 }
-
 from 'mdb-react-ui-kit';
-
 import '../styles/loginPage.css';
-// import {useMutation} from '@apollo/client'
-// import { LOGIN } from '@apollo/client'
+import {useMutation} from '@apollo/client'
+import { LOGIN } from '../utils/loginMutation'
 // import Auth from '../utils/auth';
 
 
 export default function Login() {
 
-    // const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-    // const [validated] = useState(false);
-    // const [showAlert, setShowAlert] = useState(false);
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [login, { error}] = useMutation(LOGIN);
+
   
-    // const [login, { error }] = useMutation(LOGIN);
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setUserFormData({
+         ...userFormData, 
+         [name]: value 
+        });
+    };
   
-    // useEffect(() => {
-    //   if (error) {
-    //     setShowAlert(true);
-    //   } else {
-    //     setShowAlert(false);
-    //   }
-    // }, [error]);
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
   
-    // const handleInputChange = (event) => {
-    //   const { name, value } = event.target;
-    //   setUserFormData({ ...userFormData, [name]: value });
-    // };
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
   
-    // const handleFormSubmit = async (event) => {
-    //   event.preventDefault();
+      try {
+        console.log(userFormData)
+        const { data } = await login({
+          variables: { ...userFormData },
+        });
   
-    //   const form = event.currentTarget;
-    //   if (form.checkValidity() === false) {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //   }
+        console.log(data);
+        // Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
   
-    //   try {
-    //     const { data } = await login({
-    //       variables: { ...userFormData },
-    //     });
-  
-    //     console.log(data);
-    //     Auth.login(data.login.token);
-    //   } catch (e) {
-    //     console.error(e);
-    //   }
-  
-    //   // clear form values
-    //   setUserFormData({
-    //     email: '',
-    //     password: '',
-    //   });
+      // clear form values
+      setUserFormData({
+        email: '',
+        password: '',
+      });
   
   
-    // }
+    }
 
 
 
@@ -89,9 +81,10 @@ export default function Login() {
           <MDBCol md='6'>
           <MDBTypography className='text-center h1'>Positive Thoughts</MDBTypography>
             <MDBCardBody>
-                         
-              <MDBInput className='col-xs-2' wrapperClass='mb-4' label='email address' id='typeEmail' type='email'/>
-              <MDBInput wrapperClass='mb-4' label='password' id='typePassword' type='password'/>
+                <form onSubmit={handleFormSubmit}>
+                  
+              <MDBInput name='email' onChange={handleInputChange} className='col-xs-2' wrapperClass='mb-4' label='email address' id='typeEmail' type='email'/>
+              <MDBInput name='password'  onChange={handleInputChange} wrapperClass='mb-4' label='password' id='typePassword' type='password'/>
               
               <div className="d-flex justify-content-between mx-4 mb-4">
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
@@ -100,6 +93,8 @@ export default function Login() {
 
               <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
               <MDBBtn className="mb-4 w-100" href="/signUp">Sign up</MDBBtn>
+                </form>         
+             
 
             </MDBCardBody>
 
--- client/src/utils/loginMutation.js
+++ client/src/utils/loginMutation.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 
-export const LOGIN = gql`mutation Mutation($email: String!, $password: String!) {
+export const LOGIN = gql`mutation login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
     token
     user {
