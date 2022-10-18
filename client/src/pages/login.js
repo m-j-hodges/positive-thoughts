2dd6945f36f96af8926f8b8abd9be55c70ef8361
Sean Conrad <sean.w.conrad@gmail.com>
2022-10-18 10:25:23 -0400

    adding proxy to client package.json, and fixed react-dom error in the console

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import image from '../images/PositiveThoughts.bmp'
+import { Link } from 'react-router-dom';
+import '../styles/loginPage.css';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/loginMutation';
+
+import Auth from '../utils/auth';
 // import {
 //   MDBBtn,
 //   MDBTypography,
@@ -13,82 +19,77 @@ import image from '../images/PositiveThoughts.bmp'
 //   MDBCheckbox,
 // }
 // from 'mdb-react-ui-kit';
-import '../styles/loginPage.css';
-import {useMutation} from '@apollo/client'
-import { LOGIN } from '../utils/loginMutation'
-import Auth from '../utils/auth';
 
 
-export default function Login() {
 
-    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-    const [login, { error}] = useMutation(LOGIN);
+const Login = (props) => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN);
 
+
+
+  const handleInputChange = (event) => {
   
-    const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setUserFormData({
-         ...userFormData, 
-         [name]: value 
-        });
-    };
-  
-    const handleFormSubmit = async (event) => {
+    const { name, value } = event.target;
+
+    setUserFormData({
+      ...userFormData,
+      [name]: value,
+    });
+  };
+  //submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+console.log("hi");
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
       event.preventDefault();
-  
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
-      try {
-        console.log(userFormData)
-        const { data } = await login({
-          variables: { ...userFormData },
-        });
-  
-        console.log(data);
-        Auth.login(data.login.token);
-      } catch (e) {
-        console.error(e);
-      }
-  
-      // clear form values
-      setUserFormData({
-        email: '',
-        password: '',
-      });
-  
-  
+      event.stopPropagation();
     }
 
+    try {
+      console.log(userFormData)
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+
+      console.log(data);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setUserFormData({
+      email: '',
+      password: '',
+    });
+  };
 
 
 
+  return (
 
-   return (
-    
     <div className='my-5'>
-       
-      
-        <div className='card row '>
-        
-          <div className="col" md='5'>
-            <img src={image} alt={"login"}  width={250}
-              height={250}  className=''/>
-          </div>
 
-          <div className="card-body" md='6'>
+
+      <div className='card row '>
+
+        <div className="col" md='5'>
+          <img src={image} alt={"login"} width={250}
+            height={250} className='' />
+        </div>
+
+        <div className="card-body" md='6'>
           <h5 className='text-center h1'>Positive Thoughts</h5>
-           
-                <form onSubmit={handleFormSubmit}>
-                  
-              <input name='email' onChange={handleInputChange} className='col-xs-2'  label='email address' id='typeEmail' type='email'/>
-              <input name='password'  onChange={handleInputChange}  label='password' id='typePassword' type='password'/>
 
-              <button className="mb-4 w-100">Sign in</button>
-              <button className="mb-4 w-100" href="/signUp">Sign up</button>
+          <form onSubmit={handleFormSubmit}>
+
+            <input name='email' onChange={handleInputChange} className='col-xs-2' label='email address' id='typeEmail' type='email' />
+            <input name='password' onChange={handleInputChange} label='password' id='typePassword' type='password' />
+
+            <button className="mb-4 w-100">Sign in</button>
+            <button className="mb-4 w-100" href="/signUp">Sign up</button>
                 </form>         
 
           </div>
@@ -98,4 +99,6 @@ export default function Login() {
       </div>
   
   );
-}
+
+};
+    export default Login;
