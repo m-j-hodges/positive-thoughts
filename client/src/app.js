44001651627b7ea9ff975d59c88047564f8eea73
Matthew Hodges <matt.j.hodges@gmail.com>
2022-10-18 12:51:25 -0700

    added some code for authentication. hopefully this works

--- client/src/app.js
+++ client/src/app.js
@@ -4,9 +4,34 @@ import Profile from './pages/profilePage';
 import Login from './pages/login';
 import SignUp from './pages/signUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 // import thoughtsPage from './pages/thoughtsPage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  gql
+} from "@apollo/client";
 
+const httpLink = createHttpLink({
+  uri:'/graphql',
+})
 
+const authLink = setContext((_, {headers}) => {
+
+  const token = localStorage.getItem('id_token')
+  return {
+    headers: {
+      ...headers, authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 
 function App() {
@@ -29,7 +54,7 @@ function App() {
 
     return (
       
-            
+<ApolloProvider client={client}>
       <Router>
        
        <div className="container">
@@ -54,7 +79,7 @@ function App() {
        </div>
 
    </Router>
-    
+    </ApolloProvider>
     );
   }
 
--- client/src/index.js
+++ client/src/index.js
@@ -7,25 +7,15 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import ReactDOM from 'react-dom/client'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql
-} from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: `/graphql`,
-  cache: new InMemoryCache(),
-});
+
+
 
 const container = document.getElementById('root')
 
 const root = ReactDOM.createRoot(container)
 
 root.render(
-<ApolloProvider client={client}>
-<App name="positive-thoughts" />
-</ApolloProvider>,)
+<App name="positive-thoughts" />)
 
 reportWebVitals();
